cda2616f98503660b1e07c7e4f55d0f85a3d78ff
enci.javier <enci.javier@yahoo.com>
2018-11-02 17:43:55 -0300

    ejemplo insertar publicacion

--- src/main/webapp/java/com/cartelera/dao/CarteleraDAO.java
+++ src/main/webapp/java/com/cartelera/dao/CarteleraDAO.java
@@ -1,6 +1,13 @@
 package com.cartelera.dao;
 import com.cartelera.model.Cartelera;
+import com.cartelera.model.Publicacion;
 
 public class CarteleraDAO extends GenericDAOHibernateJPA<Cartelera>{
 
+
+    public void insertarPublicacion(Publicacion publicacion, Long idCartelera){
+        Cartelera cartelera = recuperar(idCartelera);
+        cartelera.agregarPublicacion(publicacion);
+        actualizar(cartelera);
+    }
 }
